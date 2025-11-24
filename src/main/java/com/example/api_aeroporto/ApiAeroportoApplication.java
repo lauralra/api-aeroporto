18c08f77734c145712e92a8a9e2cf3664549fa59
laura lara <laura.lara.1547870@sga.pucminas.br>
2025-11-24 08:18:31 -0300

    feat: adição do springboot

---
+++ src/main/java/com/example/api_aeroporto/ApiAeroportoApplication.java
@@ -0,0 +1,13 @@
+package com.example.api_aeroporto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAeroportoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAeroportoApplication.class, args);
+	}
+
+}
