d9a3a74d7892add33987015c1c15e13542586e8d
laura lara <laura.lara.1547870@sga.pucminas.br>
2025-12-03 23:21:04 -0300

    início criação controller

---
+++ src/main/java/com/example/api_aeroporto/controller/AeroportoController.java
@@ -0,0 +1,11 @@
+package com.example.api_aeroporto.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+@AllArgsConstructor
+@NoArgsConstructor
+@RequestMapping("api/v1/aeroportos")
+public class AeroportoController {
+}
